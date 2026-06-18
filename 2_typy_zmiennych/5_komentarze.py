212cf86a00756771733746f4abac032d0c1d0abb
quenrythane <artur.babinski.g@gmail.com>
2026-06-18 21:01:19 +0200

    slice stringów

---
+++ 2_typy_zmiennych/5_komentarze.py
@@ -0,0 +1,20 @@
+
+waga = 70
+""" mój komentarz """
+# wzrot musi być podany w metrach, a nie centymatrachcentymatrach
+""" wzrot musi być
+podany w
+metrach, a nie """
+wzrost = 1.80
+bmi = waga / (wzrost ** 2)
+
+
+print(bmi)
+
+print("tutaj linjka 1")
+
+# komputer / python nie czyta pustych linii
+# one są tylko dla wizualnego rozdzielenia "modułów" kodu dla człowieka
+
+
+print("tutaj linjka 2")
---
+++ 2_typy_zmiennych/6_indeksowanie_stringow.py
@@ -0,0 +1,24 @@
+word = 'Ala ma  kota'
+#       P   y  t  h  o  n
+#
+#       0   1  2  3  4  5
+#       -6 -5 -4 -3 -2 -1
+
+"""wycianie pojdeynczej znaku"""
+print(word)
+print(word[2])
+print(word[3])
+print(word[4])
+print(word[-4])
+print(word[8])
+
+"""wycianie ciąg znaków"""
+word = 'Python'
+# word[start:stop]  # start od ŁĄCZNIE, stop ROZŁĄCZNIE
+print(word[0:4])
+print(word[0:2])  # Py
+print(word[:2])  # Jeżeli chcemy od poczatku to możeme nie podawać indeksu start
+print(word[2:])  # Jeżeli chcemy do konca to możeme nie podawać indeksu stop
+print("yth:", word[1:4])
+print(word[4:1])  # tu zrobiliśmy źle - bo indeks start musi być mniejszty niż indeks stop
+print(word[-3:])
