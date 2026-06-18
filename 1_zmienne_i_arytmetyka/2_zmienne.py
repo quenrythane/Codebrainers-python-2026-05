01e9166388fa72ce93eac823e993f2fea0e5f35b
quenrythane <artur.babinski.g@gmail.com>
2026-06-18 18:18:56 +0200

    zapis po pierwszych zajęcia i nauka o zmiennych i operacjach arytmetycznych

---
+++ 1_zmienne_i_arytmetyka/2_zmienne.py
@@ -0,0 +1,9 @@
+liczba_1 = 3
+liczba_2 = 5
+
+suma = 5
+suma = 5 + 5
+suma = "fsafa"
+suma = liczba_1
+suma = liczba_1 + liczba_2
+print(suma)
---
+++ 1_zmienne_i_arytmetyka/3_operacje_arytmetyczne.py
@@ -0,0 +1,28 @@
+suma = 2 + 4
+print("Wynik dodawania:", suma, "\n")
+
+różnica = 2 - 4
+print("Wynik odejmowania:", różnica, "\n")
+
+iloczyn = 2 * 4
+print("Wynik mnożenia:", iloczyn, "\n")
+
+iloraz = 2 / 4
+print("Wynik dzielenia:", iloraz, "\n")
+
+potega = 2 ** 4
+print("Wynik potęgowania:", potega, "\n")
+
+pierwiastek = 100 ** (1/2) # pierwiastek poziom 2
+print("Wynik pierwiastek:", pierwiastek, "\n")
+
+modulo = 12 % 5  # reszta z dzielenia
+print("Modulo:", modulo, "\n")
+
+dzielenie_bez_reszty = 32 // 10  # 1.2 -> obcina po przecinku = 1
+print("dzielenie_bez_reszty:", dzielenie_bez_reszty, "\n")
+
+# kolejność działań
+wynik = 2 + 2 * 2
+print("wynik:", wynik, "\n")
+
