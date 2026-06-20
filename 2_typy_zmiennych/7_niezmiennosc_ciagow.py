d4ffda8c8308f2afd274ae4256c84fe71cc982a9
quenrythane <artur.babinski.g@gmail.com>
2026-06-20 09:56:41 +0200

    dodanie sekcji o funkcji input() oraz fstring

---
+++ 2_typy_zmiennych/7_niezmiennosc_ciagow.py
@@ -0,0 +1,12 @@
+word = 'Python '
+# word[0] = 'p'  <- to jest niedozwolone
+
+end_word = word[1:]
+new_word = 'p' + end_word
+print(new_word)
+
+x = len(word)
+print(x)
+
+print(len(word))
+
---
+++ 2_typy_zmiennych/8_input.py
@@ -0,0 +1,16 @@
+moje_imie = input("podaj swoje imie: ")
+print("Witaj " + moje_imie)
+
+moj_rok_urodzenia = input("Podaj swoj rok urodzenia: ")
+print(moje_imie + " jest z rocznika " + moj_rok_urodzenia)
+print(f"{moje_imie} jest z rocznika {moj_rok_urodzenia}")
+
+""" Ćwiczenie 1 """
+# imie_1 = przypisz swoje imie
+# imie_2 = input() - niech user wpisze swoje imie
+# z pomocą f"" wyświetl np "Ela i Artur to koledzy."
+
+osoba_1 = "Artur"
+osoba_2 = input("Podaj swoje imie: ")
+
+print(f"{osoba_1} i {osoba_2} to koledzy.")
